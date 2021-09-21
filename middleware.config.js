cadd90d8fb3f92c69d11e437a3036ae3a6883e05
TomC13K <tomas.ciernik@and.digital>
2021-09-21 12:14:00 +0100

    initial commit

---
+++ middleware.config.js
@@ -0,0 +1,30 @@
+const projectKey = process.env.VUE_APP_CTP_PROJECT_KEY
+const clientSecret = process.env.VUE_APP_CTP_CLIENT_SECRET
+const clientId = process.env.VUE_APP_CTP_CLIENT_ID
+const uri = process.env.VUE_APP_CTP_API_URL
+const scopes = process.env.VUE_APP_CTP_SCOPES
+
+
+
+
+module.exports = {
+  integrations: {
+    ct: {
+      location: '@vue-storefront/commercetools-api/server',
+      configuration: {
+        api: {
+          uri: `${uri}/${projectKey}/graphql`,
+          authHost: 'https://auth.sphere.io',
+          projectKey,
+          clientId,
+          clientSecret,
+          scopes: [
+            scopes
+          ]
+        },
+        currency: 'USD',
+        country: 'US'
+      }
+    }
+  }
+};
